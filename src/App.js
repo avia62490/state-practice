6912d3980a63c03153046eeac289f875c0674a3e
Alex Via <alexandervia@gmail.com>
2023-01-05 11:46:42 -0600

    onClick changes state, adds styling

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import React from 'react'
 import './App.css';
 
 function App() {
-  const [result, func] = React.useState("Yes")
-    console.log(result)
+    const [result, setResult] = React.useState("Yes")
+    function handleClick() {
+        setResult("No")
+    }
     return (
         <div className="state">
             <h1 className="state--title">Is state important to know?</h1>
-            <div className="state--value">
-                <h1>{result[0]}</h1>
+            <div onClick={handleClick} className="state--value">
+                <h1>{result}</h1>
             </div>
         </div>
-  );
+);
 }
 
 export default App;
